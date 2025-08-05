070c98e4a61688663e69d2b5cf576e8762d0f495
carrbruce1 <brucecarr1290@hotmail.com>
2025-08-05 20:58:26 -0300

    modificaciones del servidor

--- backend/app.js
+++ backend/app.js
@@ -2,35 +2,39 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+dotenv.config();
 
 import feedbackRoutes from './routes/feedback.routes.js';
 import adminRoutes from './routes/admin.routes.js';
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3030;
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/admin', adminRoutes);
 
+// Rutas API
+app.use('/api/admin', adminRoutes);
+app.use('/api/feedback', feedbackRoutes);
+
+// Archivos estáticos
 app.use('/css', express.static(path.join(__dirname, '../css')));
 app.use('/js', express.static(path.join(__dirname, '../js')));
 app.use('/img', express.static(path.join(__dirname, '../img')));
-// Ruta principal para servir index.html
+
+// Rutas de páginas
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.get("/admin", (req,res) => {
-    res.sendFile(path.join(__dirname, "../admin/admin.html"))
-})
-// Usar rutas API
-app.use('/api/feedback', feedbackRoutes);
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, '../admin/admin.html'));
+});
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log(`Servidor escuchando en puerto ${PORT}`);
 });
--- backend/db/db-connection.js
+++ backend/db/db-connection.js
@@ -11,7 +11,6 @@
 // });
 
 // export default pool;
-
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
