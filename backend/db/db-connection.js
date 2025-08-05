6895d20f41025ee0fe59887804135ad649e4fc4d
carrbruce1 <brucecarr1290@hotmail.com>
2025-08-05 13:33:44 -0300

    para subir al servidor

--- backend/db/db-connection.js
+++ backend/db/db-connection.js
@@ -1,10 +1,28 @@
+// import mysql from 'mysql2/promise';
+
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'admin',
+//   password: 'admin123',
+//   database: 'encuestas_db',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// export default pool;
+
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+// Cargar variables de entorno desde .env
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'admin',
-  password: 'admin123',
-  database: 'encuestas_db',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'admin',
+  password: process.env.DB_PASS || 'admin123',
+  database: process.env.DB_NAME || 'encuestas_db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
