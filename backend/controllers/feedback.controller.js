b85dd1b836ab821283ffbce919401a4323fbe5f4
carrbruce1 <brucecarr1290@hotmail.com>
2025-08-06 16:34:10 -0300

    cambio feedback

--- backend/controllers/feedback.controller.js
+++ backend/controllers/feedback.controller.js
@@ -26,7 +26,7 @@ export const guardarFeedback = async (req, res) => {
 
     let redirectUrl = null;
     if (estrellas >= 4) {
-      redirectUrl = 'https://www.google.com/maps/place/H%26Z+indumentaria/@-31.395724,-58.0209471,17z';
+      redirectUrl = 'https://www.google.com/maps/place/H%26Z+indumentaria/@-31.395724,-58.0209471,17z/data=!4m8!3m7!1s0x95ade9005f84d9cb:0x34badc5cce1397d7!8m2!3d-31.3957286!4d-58.0183722!9m1!1b1!16s%2Fg%2F11rj92xbc9?entry=ttu&g_ep=EgoyMDI1MDgwNC4wIKXMDSoASAFQAw%3D%3D';
     }
 
     res.status(201).json({
