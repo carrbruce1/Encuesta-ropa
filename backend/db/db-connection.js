910bec9af273091b979a33f62cafc63437b11612
carrbruce1 <brucecarr1290@hotmail.com>
2025-08-06 14:12:05 -0300

    modi

--- backend/db/db-connection.js
+++ backend/db/db-connection.js
@@ -20,11 +20,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "FMfauIWqYFgVXMoqbqJJzQlIrVnKvRKK", 
+  database: process.env.DB_NAME || "railway",
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
