b73039576796b59eb451e541970d6ec584770c44
carrbruce1 <brucecarr1290@hotmail.com>
2025-08-06 10:48:06 -0300

    modificaciones

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000; // <-- 3030 local, Railway usa process.env.PORT
 
 app.use(cors());
 app.use(express.json());
@@ -35,6 +35,10 @@ app.get('/admin', (req, res) => {
   res.sendFile(path.join(__dirname, '../admin/admin.html'));
 });
 
+// app.listen(PORT, () => {
+//   console.log(`Servidor escuchando en puerto ${PORT}`);
+// });
+
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en puerto ${PORT}`);
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
--- backend/db/db-connection.js
+++ backend/db/db-connection.js
@@ -11,19 +11,23 @@
 // });
 
 // export default pool;
+
+
+
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST || process.env.MYSQLHOST,
+  user: process.env.DB_USER || process.env.MYSQLUSER,
+  password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+  database: process.env.DB_NAME || process.env.MYSQLDATABASE,
+  port: process.env.DB_PORT || process.env.MYSQLPORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
 export default pool;
+
